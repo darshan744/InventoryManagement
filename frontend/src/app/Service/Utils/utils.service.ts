7f88b0411f6b816807ebd6ffa0c7527abd500d73
darshan744 <darshankumaravelk@gmail.com>
2025-07-22 22:01:10 +0530

    fix:Broken Rendering of icon names in navbar is fixed with reactivity using BehaviourSubject and Observable

--- frontend/src/app/Service/Utils/utils.service.ts
+++ frontend/src/app/Service/Utils/utils.service.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Capacitor } from '@capacitor/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { BehaviorSubject, Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
 export class UtilsService {
-  constructor() { }
-
-  isWeb() {
-    const isWeb = Capacitor.getPlatform() === 'web';
-    console.log(isWeb);
-    return isWeb;
+  web = new BehaviorSubject<boolean>(false);
+  web$ = this.web.asObservable();
+  constructor(private observer: BreakpointObserver) {
+    this.observer.observe([Breakpoints.Handset]).subscribe((result) => {
+      console.log('BreakpointObserver result:', result);
+      this.web.next(result.matches);
+    });
   }
+
+  // isWeb() {
+  //   return this.web;
+  //   // return Capacitor.getPlatform() === 'web';
+  // }
 }
