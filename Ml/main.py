f3f192597c66072c81de278aa3a32dd94dca2f38
darshan744 <darshankumaravelk@gmail.com>
2025-07-30 18:57:26 +0530

    setup: Added Fastapi to the ml side Todo : Finalize a Model for analyzing product trends and suggest the time it would be emptied

--- Ml/main.py
+++ Ml/main.py
@@ -1,1 +1,11 @@
+from fastapi import FastAPI
 
+app = FastAPI()
+
+@app.get("/")
+async def hello():
+    return {"message": "Hello, World!"}
+
+@app.get("/{item_id}")
+def read_item(id):
+    return {"item_id": id}
